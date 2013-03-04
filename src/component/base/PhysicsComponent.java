979fb757e1a9afd090fde8c455fbb5ff00961609
OlavOlseng <olseng.o@gmail.com>
2013-03-04 22:45:35 +0100

    Implemented physics component and strong coupled it to game object. we had to....

--- src/component/base/PhysicsComponent.java
+++ src/component/base/PhysicsComponent.java
@@ -5,39 +5,61 @@ import org.jbox2d.dynamics.Body;
 import org.jbox2d.dynamics.BodyDef;
 import org.jbox2d.dynamics.FixtureDef;
 
+import util.GameConstants;
 import world.GameWorld;
+import world.gameobject.GameObject;
+import world.gameobject.Transform;
 
 public abstract class PhysicsComponent extends Component {
 	
-	protected Body b;
-	protected FixtureDef fDef;
-	protected BodyDef bDef;
+	protected Body body;
+	protected FixtureDef fixtureDef;
+	protected BodyDef bodyDef;
+	protected GameWorld world;
+	protected GameObject owner;
 	
-	protected void addToWorld(GameWorld w){
+	public PhysicsComponent(GameWorld w, GameObject owner, FixtureDef fDef, BodyDef bDef) {
+		this.fixtureDef = fDef;
+		this.bodyDef = bDef;
+		this.owner = owner;
+		this.world = w;
+		owner.setPhysicsSimulated(this);
+	}
+	
+	public void addToWorld(){
 		//You need to instantiate the bodydef and fixturedef before calling this function
-		b = w.getPhysWorld().createBody(bDef);
-		b.createFixture(fDef);
-		b.setSleepingAllowed(false);
+		if(body != null)
+			throw (new IllegalStateException("Body already in simulation!"));
 		
-		//TODO implement world add
-//		w.add(this);
+		body = world.getPhysWorld().createBody(bodyDef);
+		body.createFixture(fixtureDef);
+		body.setUserData(owner);
 	}
 	
-	public void removeFromSimulation() {
-		b.getWorld().destroyBody(b);
-		b = null;
+	public void removeFromWorld() {
+		body.getWorld().destroyBody(body);
+		body = null;
 	}
 	
-	public Body getBody() {
-		return b;
+	public void setPosition(float x, float y) {
+		if (body == null)
+			return;
+		body.setTransform(new Vec2(x / GameConstants.PIXELSCALE, y / GameConstants.PIXELSCALE), body.getAngle());
 	}
 	
-	public Vec2 getPosition(){
-		return b.getTransform().position;
+	public void setOrientation(float orientation) {
+		if (body == null)
+			return;
+		body.setTransform(body.getPosition(), orientation);
+	}	
+	
+	@Override
+	public void onUpdate(GameObject gameObject, double dt) {
+		Transform t = owner.getTransform();
+		Vec2 p = body.getPosition();
 		
-	}
-	
-	public void setPosition(Vec2 position){
-		b.setTransform(position, b.getAngle());
+		t.orientation = body.getAngle();
+		t.x = p.x * GameConstants.PIXELSCALE; 
+		t.y = p.y * GameConstants.PIXELSCALE;
 	}
 }
