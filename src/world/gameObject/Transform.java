3ae602b79347c6bdac0be75778e3a4062fbcd754
OlavOlseng <olseng.o@gmail.com>
2013-03-04 20:29:37 +0100

    flushed out the transform with constructors

--- src/world/gameObject/Transform.java
+++ src/world/gameObject/Transform.java
@@ -3,6 +3,25 @@ package world.gameobject;
 import util.DepthLevel;
 
 public class Transform {
-	public float x, y, angle;
+	public float x, y, orientation;
 	public DepthLevel depth;
+	
+	public Transform() {
+		this(0,0,0, DepthLevel.TOP_LVL);
+	}
+	
+	public Transform(float x, float y) {
+		this(x, y, 0, DepthLevel.TOP_LVL);
+	}
+	
+	public Transform(float x, float y, float orientation) {
+		this(x, y, orientation, DepthLevel.TOP_LVL);
+	}
+	
+	public Transform(float x, float y, float orientation, DepthLevel dpt) {
+		this.x = x;
+		this.y = y;
+		this.orientation = orientation;
+		this.depth = dpt;
+	}
 }
