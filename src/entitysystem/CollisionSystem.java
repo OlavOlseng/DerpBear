d18ef4c9c22a466d9c3342d7b6fea699a8da171f
OlavOlseng <olseng.o@gmail.com>
2013-04-14 15:01:06 +0200

    minor cleanup in imports

--- src/entitysystem/CollisionSystem.java
+++ src/entitysystem/CollisionSystem.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 
 import org.jbox2d.dynamics.contacts.Contact;
 
-import entitysystem.component.Component;
 import entitysystem.component.DamageComponent;
 import entitysystem.component.StatusComponent;
 import entitysystem.component.PhysicsComponent;
@@ -24,37 +23,37 @@ public class CollisionSystem extends BaseSystem{
 
 	@Override
 	public void update(float dt) {
-		
+
 		ArrayList<Contact> contacts = ch.getContacts();
 		ArrayList<Entity> toBeRemoved = new ArrayList<Entity>();
+
 		for (Contact c : contacts) {
 			if(!c.isTouching()){
 				continue;
 			}
-			if (c.isTouching()){
-				PhysicsComponent a = (PhysicsComponent)(c.getFixtureA().getBody().getUserData());
-				PhysicsComponent b = (PhysicsComponent)(c.getFixtureB().getBody().getUserData());
 
-				Entity entA = a.getOwnerEntity();
-				Entity entB = b.getOwnerEntity();
+			PhysicsComponent a = (PhysicsComponent)(c.getFixtureA().getBody().getUserData());
+			PhysicsComponent b = (PhysicsComponent)(c.getFixtureB().getBody().getUserData());
 
-				DamageComponent dca = (DamageComponent)(getEntityManager().getComponentOfClassForEntity(damageComp, entA));
-				DamageComponent dcb = (DamageComponent)(getEntityManager().getComponentOfClassForEntity(damageComp, entB));
+			Entity entA = a.getOwnerEntity();
+			Entity entB = b.getOwnerEntity();
 
-				StatusComponent sca = (StatusComponent)(getEntityManager().getComponentOfClassForEntity(statusComp, entA));
-				StatusComponent scb = (StatusComponent)(getEntityManager().getComponentOfClassForEntity(statusComp, entB));
+			DamageComponent dca = (DamageComponent)(getEntityManager().getComponentOfClassForEntity(damageComp, entA));
+			DamageComponent dcb = (DamageComponent)(getEntityManager().getComponentOfClassForEntity(damageComp, entB));
 
-				if(dca != null && scb != null) {
-					scb.takeDamage(dca.getDamageAmount());
-					if(!scb.isAlive()) {
-						toBeRemoved.add(entB);
-					}
+			StatusComponent sca = (StatusComponent)(getEntityManager().getComponentOfClassForEntity(statusComp, entA));
+			StatusComponent scb = (StatusComponent)(getEntityManager().getComponentOfClassForEntity(statusComp, entB));
+
+			if(dca != null && scb != null) {
+				scb.takeDamage(dca.getDamageAmount());
+				if(!scb.isAlive()) {
+					toBeRemoved.add(entB);
 				}
-				if(dcb != null && sca != null) {
-					sca.takeDamage(dcb.getDamageAmount());
-					if(!sca.isAlive()) {
-						toBeRemoved.add(entA);
-					}
+			}
+			if(dcb != null && sca != null) {
+				sca.takeDamage(dcb.getDamageAmount());
+				if(!sca.isAlive()) {
+					toBeRemoved.add(entA);
 				}
 			}
 		}
